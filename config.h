ce8efc266b384cb92ebcbff9b57bfa775aca66c6
jules-franko <julesfranko@proton.me>
2025-06-02 00:23:06 -0600

    adjustments

--- config.h
+++ config.h
@@ -94,8 +94,9 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.80;
+float alpha = 0.85;
 
+#include "/home/jules/.cache/wal/colors-wal-st.h"
 
 /* Terminal colors (16 first used in escape sequence) */
 //static const char *colorname[] = {
@@ -129,44 +130,6 @@ float alpha = 0.80;
 
 
 //};
-
-
-static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0]  = "#1e1e1e", /* black   */
-  [1]  = "#f7768e", /* red     */
-  [2]  = "#9ece6a", /* green   */
-  [3]  = "#e0af68", /* yellow  */
-  [4]  = "#7aa2f7", /* blue    */
-  [5]  = "#bb9af7", /* magenta */
-  [6]  = "#7dcfff", /* cyan    */
-  [7]  = "#a9b1d6", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#414868", /* black   */
-  [9]  = "#f7768e", /* red     */
-  [10] = "#9ece6a", /* green   */
-  [11] = "#e0af68", /* yellow  */
-  [12] = "#7aa2f7", /* blue    */
-  [13] = "#bb9af7", /* magenta */
-  [14] = "#7dcfff", /* cyan    */
-  [15] = "#dcdfe4", /* white   */
-
-  /* special colors */
-  [257] = "#1e1e1e", /* background */
-  [256] = "#a9b1d6", /* foreground */
-};
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
-
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
